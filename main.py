4240d42fb3c3c3a3efa53726c9ad778f62b48e24
bertmad3400 <skrivtilbertram@gmail.com>
2021-10-16 15:30:04 +0200

    Organize code
    Move code for configuring the driver into own function, so it can be
    called after the driver has been downloaded, and move the rest of the
    code into a main function.

--- main.py
+++ main.py
@@ -29,9 +29,6 @@ password = "Testpassword"
 limitNumber = 20
 profiles = ["bleepingcomputer", "zdnet", "threatpost"]
 
-# Setting the options for running the browser driver headlessly so it doesn't pop up when running the script
-#driverOptions = Options()
-#driverOptions.headless = headless
 
 # Mozilla will have an api endpoint giving a lot of information about the latest releases for the geckodriver, from which the url for the linux 64 bit has to be extracted
 def extractDriverURL():
@@ -47,84 +44,107 @@ def downloadDriver(driverURL):
     with tarfile.open(fileobj=driverContents.raw, mode='r|gz') as driverFile:
         driverFile.extractall()
 
-    print("Page loads") if driver.find_element_by_id("links").is_enabled() else exit()
+def configureDriver():
+    # Setting the options for running the browser driver headlessly so it doesn't pop up when running the script
+    #driverOptions = Options()
+    #driverOptions.headless = headless
 
-    driver.get(f"{url}/signup")
-    driver.find_element_by_id("username").send_keys(username);
-    driver.find_element_by_id("password").send_keys(password);
-    driver.find_element_by_id("confirmPassword").send_keys(password);
-    submitButton = driver.find_element_by_id("submit")
-    submitButton.click()
+    # Setup the webdriver with options
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("permissions.default.image", 2)
+    return webdriver.Firefox(executable_path="./geckodriver", firefox_profile=profile)
 
-    print("User created successfully") if driver.find_elements_by_class_name("error-message")[0].text == "Created user, log in here." else exit()
+def main(driver):
 
-    driver.get(f"{url}/signup")
-    driver.find_element_by_id("username").send_keys(username);
-    driver.find_element_by_id("password").send_keys(password + "g");
-    driver.find_element_by_id("confirmPassword").send_keys(password + "g");
-    submitButton = driver.find_element_by_id("submit")
-    submitButton.click()
+    for url in urls:
+        print(f"\n\n---\n\nTesting {url}\n\n--\n\n")
+        driver.get(url)
 
-    print("Confirmed that the same user can't be created twice") if driver.find_elements_by_class_name("error-message")[0].text == "User already exists, log in here." else exit()
+        print("Page loads") if driver.find_element_by_id("links").is_enabled() else exit()
 
-    driver.find_element_by_id("username").send_keys(username);
-    driver.find_element_by_id("password").send_keys(password);
-    submitButton = driver.find_element_by_id("submit")
-    submitButton.click()
+        driver.get(f"{url}/signup")
+        driver.find_element_by_id("username").send_keys(username);
+        driver.find_element_by_id("password").send_keys(password);
+        driver.find_element_by_id("confirmPassword").send_keys(password);
+        submitButton = driver.find_element_by_id("submit")
+        submitButton.click()
 
-    print("User logged in successfully") if len(driver.find_elements_by_class_name("listview-item-checkbutton")) > 5 else exit()
+        print("User created successfully") if driver.find_elements_by_class_name("error-message")[0].text == "Created user, log in here." else exit()
 
-    driver.get(f"{url}/config")
+        driver.get(f"{url}/signup")
+        driver.find_element_by_id("username").send_keys(username);
+        driver.find_element_by_id("password").send_keys(password + "g");
+        driver.find_element_by_id("confirmPassword").send_keys(password + "g");
+        submitButton = driver.find_element_by_id("submit")
+        submitButton.click()
 
-    for profile in profiles:
-        driver.find_element_by_css_selector(f"label[for='{profile}-checkbox']").click()
+        print("Confirmed that the same user can't be created twice") if driver.find_elements_by_class_name("error-message")[0].text == "User already exists, log in here." else exit()
 
-    driver.find_element_by_class_name("form-control").send_keys(Keys.CONTROL, "a")
-    driver.find_element_by_class_name("form-control").send_keys(Keys.BACKSPACE)
-    driver.find_element_by_class_name("form-control").send_keys(str(limitNumber))
-    driver.find_element_by_css_selector("label[for='readingmode-checkbox']").click()
-    driver.find_element_by_css_selector("form[action='/']").submit()
+        driver.find_element_by_id("username").send_keys(username);
+        driver.find_element_by_id("password").send_keys(password);
+        submitButton = driver.find_element_by_id("submit")
+        submitButton.click()
 
-    # Need to be here to allow the page to load properly
-    time.sleep(1)
+        print("User logged in successfully") if len(driver.find_elements_by_class_name("listview-item-checkbutton")) > 5 else exit()
 
-    print(len(driver.find_elements_by_class_name("col-sm-12")))
-    print("Limiting the number of articles work") if len(driver.find_elements_by_class_name("col-sm-12")) == limitNumber else exit()
+        driver.get(f"{url}/config")
 
-    DetailPatterns = {
-            "publisher":    re.compile(r'(?<=Publisher: )\w*'),
-            "publishDate":  re.compile(r'(?<=Published: ).*')
-            }
+        for profile in profiles:
+            driver.find_element_by_css_selector(f"label[for='{profile}-checkbox']").click()
+
+        driver.find_element_by_class_name("form-control").send_keys(Keys.CONTROL, "a")
+        driver.find_element_by_class_name("form-control").send_keys(Keys.BACKSPACE)
+        driver.find_element_by_class_name("form-control").send_keys(str(limitNumber))
+        driver.find_element_by_css_selector("label[for='readingmode-checkbox']").click()
+        driver.find_element_by_css_selector("form[action='/']").submit()
+
+        # Need to be here to allow the page to load properly
+        time.sleep(1)
+
+        print(len(driver.find_elements_by_class_name("col-sm-12")))
+        print("Limiting the number of articles work") if len(driver.find_elements_by_class_name("col-sm-12")) == limitNumber else exit()
+
+        DetailPatterns = {
+                "publisher":    re.compile(r'(?<=Publisher: )\w*'),
+                "publishDate":  re.compile(r'(?<=Published: ).*')
+                }
 
 
-    print(f"Verifying {limitNumber} elements")
-    lastTime = datetime.now(timezone.utc).astimezone()
-    for i in range(len(driver.find_elements_by_class_name("col-sm-12"))):
-        print(i, end=" ")
+        print(f"Verifying {limitNumber} elements")
+        lastTime = datetime.now(timezone.utc).astimezone()
+        for i in range(len(driver.find_elements_by_class_name("col-sm-12"))):
+            print(i, end=" ")
 
-        currentElementDesc = driver.find_elements_by_class_name("source")[i].text
-        currentPublisher = DetailPatterns['publisher'].search(currentElementDesc).group(0)
-        currentTime = datetime.strptime(DetailPatterns['publishDate'].search(currentElementDesc).group(0), '%Y-%m-%d %H:%M:%S%z')
+            currentElementDesc = driver.find_elements_by_class_name("source")[i].text
+            currentPublisher = DetailPatterns['publisher'].search(currentElementDesc).group(0)
+            currentTime = datetime.strptime(DetailPatterns['publishDate'].search(currentElementDesc).group(0), '%Y-%m-%d %H:%M:%S%z')
 
-        if currentPublisher not in profiles: print("\nPublisher-check failed"); exit()
-        if currentTime > lastTime: print("\nDate-check failed"); exit()
-        lastTime = currentTime
+            if currentPublisher not in profiles: print("\nPublisher-check failed"); exit()
+            if currentTime > lastTime: print("\nDate-check failed"); exit()
+            lastTime = currentTime
 
-    print("\nLimiting sources work")
-    print("Sorts properly by date")
+        print("\nLimiting sources work")
+        print("Sorts properly by date")
 
-    print(f"Verifying reading-mode of {limitNumber} elements")
-    for i,element in enumerate(driver.find_elements_by_class_name("col-sm-12")):
-        print(i, end=" ")
+        print(f"Verifying reading-mode of {limitNumber} elements")
+        for i,element in enumerate(driver.find_elements_by_class_name("col-sm-12")):
+            print(i, end=" ")
 
-        linkLocation = element.find_element_by_xpath("./child::a").get_attribute("href")
+            linkLocation = element.find_element_by_xpath("./child::a").get_attribute("href")
 
-        driver.get(linkLocation)
-        if len(driver.find_elements_by_css_selector("li")) < 4: print(f"Reading mode doesn't seem to render properly for {linkLocation}"); exit()
-        driver.back()
+            driver.get(linkLocation)
+            if len(driver.find_elements_by_css_selector("li")) < 4: print(f"Reading mode doesn't seem to render properly for {linkLocation}"); exit()
+            driver.back()
 
-    print("Reading mode works")
+        print("\nReading mode works")
 
-    driver.get(f"{url}/logout")
+        driver.get(f"{url}/logout")
 
-    print("Logged out successfuly") if driver.find_elements_by_class_name("listview-item-checkbutton") == [] else exit()
+        print("Logged out successfuly") if driver.find_elements_by_class_name("listview-item-checkbutton") == [] else exit()
+
+if __name__ == "__main__":
+    if not os.path.isfile("./geckodriver"):
+        downloadDriver(extractDriverURL())
+    driver = configureDriver()
+    main(driver)
+
