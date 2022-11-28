5239eea02b161bf9ade34ef4332c47bb54bedc37
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-28 03:31:35 +0000

    Adding tarfile member sanitization to extractall()

--- main.py
+++ main.py
@@ -42,7 +42,26 @@ def extractDriverURL():
 def downloadDriver(driverURL):
     driverContents = requests.get(driverURL, stream=True)
     with tarfile.open(fileobj=driverContents.raw, mode='r|gz') as driverFile:
-        driverFile.extractall()
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(driverFile)
 
 def configureDriver():
     # Setting the options for running the browser driver headlessly so it doesn't pop up when running the script
